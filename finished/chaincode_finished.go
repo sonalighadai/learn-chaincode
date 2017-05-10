7bbf4c3fa118b2b67fc6d261616b49643d4587bc
sonalighadai <sonali.ghadai@gmail.com>
2017-05-10 12:11:05 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -100,6 +100,9 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 
 	key = args[0] //rename for funsies
 	value = args[1]
+	fmt.Println(value)
+	strB, _ := json.Marshal(value)
+	fmt.Println(string(strB))
 	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 	if err != nil {
 		return nil, err
